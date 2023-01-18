f57a6bfca093c93628967e930955e8337571c08b
tamakui <122041014+tamakui@users.noreply.github.com>
2023-01-18 10:06:48 +0000

    tuika

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,11 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
   
   def create
-   book = Book(book_params)
+   book = Book.new(book_params)
    book.save
   redirect_to "/"
   end
